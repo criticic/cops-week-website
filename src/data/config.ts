730eca29212cbf4206cf13d47fa999134995e773
Sagnik Mandal <acriticalcynic@outlook.com>
2025-02-12 21:33:15 +0530

    basic structure

---
+++ src/data/config.ts
@@ -0,0 +1,9 @@
+export const SITE_CONFIG = {
+  title: "COPS Week 2025",
+  description: "The official site for COPS Week 2025 at IIT BHU, featuring events, workshops, and hackathons!",
+  social: {
+    github: "https://github.com/COPS-IITBHU",
+    linkedin: "https://www.linkedin.com/company/cops-iitbhu/",
+    instagram: "https://www.instagram.com/cops.iitbhu/"
+  }
+};
