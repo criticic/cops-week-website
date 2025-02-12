0effebb5e53687450fd3f22d1a4e8984fb9dec92
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2025-02-12 23:54:22 +0530

    add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,10 @@
 export const SITE_CONFIG = {
-  title: "COPS Week 2025",
-  description: "The official site for COPS Week 2025 at IIT BHU, featuring events, workshops, and hackathons!",
+  title: 'COPS Week 2025',
+  description:
+    'The official site for COPS Week 2025 at IIT BHU, featuring events, workshops, and hackathons!',
   social: {
-    github: "https://github.com/COPS-IITBHU",
-    linkedin: "https://www.linkedin.com/company/cops-iitbhu/",
-    instagram: "https://www.instagram.com/cops.iitbhu/"
-  }
+    github: 'https://github.com/COPS-IITBHU',
+    linkedin: 'https://www.linkedin.com/company/cops-iitbhu/',
+    instagram: 'https://www.instagram.com/cops.iitbhu/',
+  },
 };
